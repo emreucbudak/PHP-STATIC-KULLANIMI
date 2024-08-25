203afb6a982e3595b928e95a1b958012587ca14e
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-25 22:49:50 +0300

    Add files via upload

---
+++ main (23).php
@@ -0,0 +1,31 @@
+<?php
+/******************************************************************************
+
+Emre Üçbudak Karadeniz Teknik Üniversitesi
+
+*******************************************************************************/
+// PHP FONKSIYONLARA GIRIS
+// php static kullanımı
+
+function islem() {
+    static $deger = 1;
+    $deger++;
+    echo($deger);
+}
+islem();
+islem();
+islem();
+islem();
+islem();
+islem();
+islem();
+islem();
+islem();
+islem();
+islem();
+
+
+
+
+
+?>
